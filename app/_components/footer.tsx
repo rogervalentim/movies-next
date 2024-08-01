eb5e229eac223a3923ebafddf3eb6fe7487691e6
rogervalentim <valentim.roger33@gmail.com>
2024-08-01 20:08:50 -0300

    feat: add footer component

---
+++ app/_components/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <div className="flex justify-center gap-2 items-center py-4">
+      <h1>Desenvolvido por</h1>{" "}
+      <a href="https://github.com/rogervalentim/" target="_blank">
+        Roger Valentim
+      </a>
+    </div>
+  );
+}
